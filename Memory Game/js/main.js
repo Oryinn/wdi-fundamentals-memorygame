9dea7920c9fa1c9ce0c88991b6f1b0a6fccdff6f
Jordan Williams <Oryinn@Jordans-MBP-6.attlocal.net>
2019-01-20 10:07:32 -0500

    created game logic

--- Memory Game/js/main.js
+++ Memory Game/js/main.js
@@ -1,7 +1,17 @@
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
+var cardOne = cards[0];
+cardsInPlay.push(cardOne);
+console.log("User flipped Queen");
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log(cardsInPlay[1]);
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+if (cardsInPlay.length === 2) {
+	}if (cardsInPlay[0] === cardsInPlay[1]){ 
+		alert("You found a match!");
+	}
+	else {
+		alert("Sorry, try again.");
+	}
+
